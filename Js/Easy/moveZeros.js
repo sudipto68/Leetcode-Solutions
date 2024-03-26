9d9b1cf41bc3d5e4c2a3db325e3e297329dd88eb
sudipto68 <sudiptomitro2016@gmail.com>
2024-03-27 01:04:19 +0600

    Move zeros solved

---
+++ Js/Easy/moveZeros.js
@@ -0,0 +1,13 @@
+var moveZeroes = function (nums) {
+  let left = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] !== 0) {
+      [nums[right], nums[left]] = [nums[left], nums[right]];
+      left++;
+    }
+  }
+
+  return nums;
+};
+console.log(moveZeroes([0, 0, 1]));
